5d644160acb1e718f748b28b7868baa0fe40d3c3
ng-hai <nghai.dev@gmail.com>
2020-01-23 16:53:41 +0700

    Update image path

--- src/components/seo.js
+++ src/components/seo.js
@@ -15,27 +15,28 @@ export default function SEO({
   slug,
 }) {
   const url = `${config.url}${slug}`
+  const previewImage = `${config.url}${image}`
   return (
     <Head>
       {/* General tags */}
       <title>{title}</title>
       <meta name="title" content={title} />
       <meta name="description" content={description} />
-      <meta name="image" content={image} />
+      <meta name="image" content={previewImage} />
 
       {/* OpenGraph tags */}
       <meta property="og:url" content={url} />
       <meta property="og:type" content={slug ? "article" : "website"} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
+      <meta property="og:image" content={previewImage} />
 
       {/* Twitter Card tags */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={config.author} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image} />
+      <meta name="twitter:image" content={previewImage} />
     </Head>
   )
 }
